fa8a638ad860a62b71cfb61b738d80b7be64522a
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-06-11 15:51:10 +0900

    Update lib.rs

--- crates/egui-winit/src/lib.rs
+++ crates/egui-winit/src/lib.rs
@@ -828,25 +828,21 @@ impl State {
             self.clipboard.set(copied_text);
         }
 
-        let allow_ime = ime.is_some();
-        if self.allow_ime != allow_ime {
-            self.allow_ime = allow_ime;
-            crate::profile_scope!("set_ime_allowed");
-            window.set_ime_allowed(allow_ime);
-        }
-
         if let Some(ime) = ime {
-            let pixels_per_point = pixels_per_point(&self.egui_ctx, window);
-            let ime_rect_px = ime.rect * pixels_per_point;
-            let mut need_set_ime_cursor_area = true;
-
-            // On Wayland of Linux, repaints every frame Issue : See https://github.com/emilk/egui/pull/4254
-            if self.egui_ctx.os() == egui::os::OperatingSystem::Nix {
-                need_set_ime_cursor_area = self.ime_rect_px != Some(ime_rect_px)
-                    && self.egui_ctx.input(|i| !i.events.is_empty());
+            if self.allow_ime != ime.allowed_ime {
+                self.allow_ime = ime.allowed_ime;
+                crate::profile_scope!("set_ime_allowed");
+                window.set_ime_allowed(self.allow_ime);
             }
 
-            if need_set_ime_cursor_area {
+            let pixels_per_point = pixels_per_point(&self.egui_ctx, window);
+            let ime_rect_px = ime.cursor_rect * pixels_per_point;
+
+            let is_need_cursor_area = self.ime_rect_px != Some(ime_rect_px)
+                && self.egui_ctx.input(|i| !i.events.is_empty());
+
+            if ime.visible && is_need_cursor_area {
+                self.ime_rect_px = Some(ime_rect_px);
                 crate::profile_scope!("set_ime_cursor_area");
                 window.set_ime_cursor_area(
                     winit::dpi::PhysicalPosition {
@@ -858,13 +854,9 @@ impl State {
                         height: ime_rect_px.height(),
                     },
                 );
-
-                self.ime_rect_px = Some(ime_rect_px);
             } else {
                 self.ime_rect_px = None;
             }
-        } else {
-            self.ime_rect_px = None;
         }
 
         #[cfg(feature = "accesskit")]
@@ -1475,6 +1467,12 @@ fn process_viewport_command(
                 PhysicalPosition::new(ime_rect_px.min.x, ime_rect_px.min.y),
                 PhysicalSize::new(ime_rect_px.width(), ime_rect_px.height()),
             );
+
+            egui_ctx.output_mut(|o| {
+                if let Some(ime) = &mut o.ime {
+                    ime.cursor_rect = ime_rect;
+                }
+            });
         }
         ViewportCommand::IMEAllowed(v) => window.set_ime_allowed(v),
         ViewportCommand::IMEPurpose(p) => window.set_ime_purpose(match p {
