8f850fdae7849153fb88f56444335cce521e940e
rustbasic <127506429+rustbasic@users.noreply.github.com>
2024-06-29 11:35:44 +0900

    Update lib.rs

--- crates/egui-winit/src/lib.rs
+++ crates/egui-winit/src/lib.rs
@@ -444,7 +444,7 @@ impl State {
                 self.egui_input.modifiers.alt = alt;
                 self.egui_input.modifiers.ctrl = ctrl;
                 self.egui_input.modifiers.shift = shift;
-                self.egui_input.modifiers.mac_cmd = cfg!(target_os = "macos") && super_;
+                self.egui_input.modifiers.mac_cmd = super_;
                 self.egui_input.modifiers.command = if cfg!(target_os = "macos") {
                     super_
                 } else {
@@ -828,34 +828,38 @@ impl State {
             self.clipboard.set(copied_text);
         }
 
-        let allow_ime = ime.is_some();
-        if self.allow_ime != allow_ime {
-            self.allow_ime = allow_ime;
-            crate::profile_scope!("set_ime_allowed");
-            window.set_ime_allowed(allow_ime);
-        }
-
         if let Some(ime) = ime {
-            let pixels_per_point = pixels_per_point(&self.egui_ctx, window);
-            let ime_rect_px = pixels_per_point * ime.rect;
-            if self.ime_rect_px != Some(ime_rect_px)
-                || self.egui_ctx.input(|i| !i.events.is_empty())
-            {
-                self.ime_rect_px = Some(ime_rect_px);
-                crate::profile_scope!("set_ime_cursor_area");
-                window.set_ime_cursor_area(
-                    winit::dpi::PhysicalPosition {
-                        x: ime_rect_px.min.x,
-                        y: ime_rect_px.min.y,
-                    },
-                    winit::dpi::PhysicalSize {
-                        width: ime_rect_px.width(),
-                        height: ime_rect_px.height(),
-                    },
+            if self.allow_ime != ime.allowed_ime {
+                self.allow_ime = ime.allowed_ime;
+                crate::profile_scope!("set_ime_allowed");
+                self.egui_ctx.send_viewport_cmd_to(
+                    self.viewport_id,
+                    ViewportCommand::IMEAllowed(self.allow_ime),
                 );
             }
-        } else {
-            self.ime_rect_px = None;
+
+            if ime.ime_enabled && ime.visible {
+                let pixels_per_point = pixels_per_point(&self.egui_ctx, window);
+                let ime_rect_px = ime.cursor_rect * pixels_per_point;
+
+                let is_need_cursor_area = self.ime_rect_px != Some(ime_rect_px)
+                    && self
+                        .egui_ctx
+                        .input_for(self.viewport_id, |i| !i.events.is_empty());
+
+                if is_need_cursor_area {
+                    self.ime_rect_px = Some(ime_rect_px);
+                    crate::profile_scope!("set_ime_cursor_area");
+                    self.egui_ctx.send_viewport_cmd_to(
+                        self.viewport_id,
+                        ViewportCommand::IMERect(ime.cursor_rect),
+                    );
+                } else {
+                    self.ime_rect_px = None;
+                }
+            } else {
+                self.ime_rect_px = None;
+            }
         }
 
         #[cfg(feature = "accesskit")]
@@ -973,6 +977,7 @@ pub fn update_viewport_info(
     }
 
     viewport_info.fullscreen = Some(window.fullscreen().is_some());
+    viewport_info.decorations = Some(window.is_decorated());
     viewport_info.focused = Some(window.has_focus());
 }
 
@@ -1309,10 +1314,20 @@ fn process_viewport_command(
 
     match command {
         ViewportCommand::Close => {
+            info.close_requested_on();
+            info.close_cancelable_off();
             info.events.push(egui::ViewportEvent::Close);
         }
         ViewportCommand::CancelClose => {
             // Need to be handled elsewhere
+            info.close_requested_off();
+            if let Some(position) = info
+                .events
+                .iter()
+                .position(|x| *x == egui::ViewportEvent::Close)
+            {
+                info.events.remove(position);
+            }
         }
         ViewportCommand::StartDrag => {
             // If `.has_focus()` is not checked on x11 the input will be permanently taken until the app is killed!
@@ -1365,7 +1380,11 @@ fn process_viewport_command(
         ViewportCommand::Title(title) => {
             window.set_title(&title);
         }
-        ViewportCommand::Transparent(v) => window.set_transparent(v),
+        ViewportCommand::Transparent(v) => {
+            window.set_transparent(v);
+            // remember it here. because it is difficult to check in `update_viewport_info()`.
+            info.transparent = Some(v);
+        }
         ViewportCommand::Visible(v) => window.set_visible(v),
         ViewportCommand::OuterPosition(pos) => {
             window.set_outer_position(PhysicalPosition::new(
@@ -1410,16 +1429,20 @@ fn process_viewport_command(
         ),
         ViewportCommand::Minimized(v) => {
             window.set_minimized(v);
+            // remember it here. because it is difficult to check in `update_viewport_info()`.
             info.minimized = Some(v);
         }
         ViewportCommand::Maximized(v) => {
             window.set_maximized(v);
+            // remember it here. because it is difficult to check in `update_viewport_info()`.
             info.maximized = Some(v);
         }
         ViewportCommand::Fullscreen(v) => {
             window.set_fullscreen(v.then_some(winit::window::Fullscreen::Borderless(None)));
         }
-        ViewportCommand::Decorations(v) => window.set_decorations(v),
+        ViewportCommand::Decorations(v) => {
+            window.set_decorations(v);
+        }
         ViewportCommand::WindowLevel(l) => window.set_window_level(match l {
             egui::viewport::WindowLevel::AlwaysOnBottom => WindowLevel::AlwaysOnBottom,
             egui::viewport::WindowLevel::AlwaysOnTop => WindowLevel::AlwaysOnTop,
@@ -1429,13 +1452,11 @@ fn process_viewport_command(
             let winit_icon = icon.and_then(|icon| to_winit_icon(&icon));
             window.set_window_icon(winit_icon);
         }
-        ViewportCommand::IMERect(rect) => {
+        ViewportCommand::IMERect(ime_rect) => {
+            let ime_rect_px = ime_rect * pixels_per_point;
             window.set_ime_cursor_area(
-                PhysicalPosition::new(pixels_per_point * rect.min.x, pixels_per_point * rect.min.y),
-                PhysicalSize::new(
-                    pixels_per_point * rect.size().x,
-                    pixels_per_point * rect.size().y,
-                ),
+                PhysicalPosition::new(ime_rect_px.min.x, ime_rect_px.min.y),
+                PhysicalSize::new(ime_rect_px.width(), ime_rect_px.height()),
             );
         }
         ViewportCommand::IMEAllowed(v) => window.set_ime_allowed(v),
